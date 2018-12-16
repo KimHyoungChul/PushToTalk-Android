4c50f2ab66ed4e99af71a0c8f185e6ab59e4849e
Matt Roberts <mattlroberts@live.com>
2018-12-16 15:48:55 -0800

    created Media Manager to start and stop recordings

---
+++ app/src/main/java/com/unearth/pushtotalk/MediaManager.java
@@ -0,0 +1,38 @@
+package com.unearth.pushtotalk;
+
+import android.media.MediaRecorder;
+import android.util.Log;
+
+import java.io.IOException;
+
+/**
+ * Created by Matthew Roberts on 12/16/2018.
+ */
+public class MediaManager {
+
+    private MediaRecorder mRecorder;
+
+    public MediaManager() {}
+
+    public void startRecording(String fileName) {
+        mRecorder = new MediaRecorder();
+        mRecorder.setAudioSource(MediaRecorder.AudioSource.MIC);
+        mRecorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
+        mRecorder.setOutputFile(fileName);
+        mRecorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
+
+        try {
+            mRecorder.prepare();
+        } catch (IOException e) {
+            Log.e(MediaManager.class.getSimpleName(), "prepare() failed");
+        }
+
+        mRecorder.start();
+    }
+
+    public void stopRecording() {
+        mRecorder.stop();
+        mRecorder.release();
+        mRecorder = null;
+    }
+}
